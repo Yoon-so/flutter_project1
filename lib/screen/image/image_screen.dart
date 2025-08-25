cbe7b0bceab11aa57617f02f1aef675a9fc861cb
Ekaterina <yoonrin113@gmail.com>
2025-08-26 08:13:38 +0900

    Add: image_screen.dart assets file

--- lib/screen/image/image_screen.dart
+++ lib/screen/image/image_screen.dart
@@ -35,16 +35,6 @@ class ImageScreen extends StatelessWidget {
                 height: 300,
                 child: Image.network(
                     'https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png',
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                color: Colors.grey,
-                width: 300,
-                height: 300,
-                child: Image.network(
-                    'https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png',
                   fit: BoxFit.fitWidth,
                 ),
               ),
@@ -60,6 +50,7 @@ class ImageScreen extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 30),
+              SizedBox(width: 150, height: 150, child: Image.asset('assets/lecture_flutter_image'),),
               SizedBox(height: 30),
             ],
           ),
