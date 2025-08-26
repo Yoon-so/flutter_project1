935ae1b1d2f45d4d91e55861c0170f4e394a9c1e
Ekaterina <yoonrin113@gmail.com>
2025-08-26 15:21:52 +0900

    Add: variable.dart

---
+++ test/variable.dart
@@ -0,0 +1,48 @@
+
+class Variable {
+  Variable() {
+    String name = '리나';
+    name = 'Rina';
+    int age = 21;
+    bool check = true;
+    bool check2 = false;
+    double weight = 67;
+
+    //var, dynamic
+    var animal = 'cat';
+    animal = 'dog';
+
+    dynamic home = false;
+    home = true;
+    home = 'my home';
+    home = 10;
+
+    String? smwu = 'yes';
+    smwu = null;
+    //Text(smwu);
+    int? age2 = 25;
+    age2 = null;
+    bool? h2 = false;
+    h2 = null;
+
+    //final, const 변수의 값 변경 불가
+    //final과 const의 차이는 -> 컴파일 시점의 차이
+    //컴파일 = 사람이 작성한 코드를 컴퓨터가 인식할 수 있게 변환하는 작업.
+    //런타임 = 프로그램을 실행하고 있는 상태.
+    //const는 컴파일 시점에 값이 정해짐.
+    //final은 런타임 시점에 값이 정해짐. -> 메모리에 올라옴.
+
+    final String name3 = 'rina1';
+    const String address = 'aaa';
+    //address = "bbb";
+
+    print('name: $name, age: $age, weight: $weight');
+    print('animal: $animal, home: $home');
+    print('nullable: smwu=$smwu, age2=$age2, h2=$h2');
+    print('final: $name3, const: $address');
+  }
+}
+
+void main() {
+  Variable();
+}
