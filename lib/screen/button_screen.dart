746ff914dadbde3362d002f476ae0f282302ee1f
Ekaterina <yoonrin113@gmail.com>
2025-08-26 14:15:15 +0900

    Add: stack_screen.dart

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -9,6 +9,7 @@ import 'package:flutter_project1/screen/row/column_row_practice_screen.dart';
 import 'package:flutter_project1/screen/row/row_practice_screen.dart';
 import 'package:flutter_project1/screen/row/row_screen.dart';
 import 'package:flutter_project1/screen/stateful/stateful_screen.dart';
+import 'package:flutter_project1/screen/stack/stack_screen.dart';
 import 'package:flutter_project1/screen/text/text_practice_screen.dart';
 import 'package:flutter_project1/screen/text/text_screen.dart';
 
@@ -174,6 +175,20 @@ class ButtonScreen extends StatelessWidget {
                 );
               },
               child: Text("Image 실습"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return StackScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Stack"),
             ),
           ],
         ),
