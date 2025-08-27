b8bb298bdf93bcb89565a4ee68a05df5406faea6
Ekaterina <yoonrin113@gmail.com>
2025-08-27 10:22:43 +0900

    Add: for_loop.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,22 +11,25 @@ import 'package:flutter_project1/main.dart';
 
 import 'collections.dart';
 import 'conditional.dart';
+import 'for_loop.dart';
 import 'method.dart';
 
 void main() {
-  Method();
-  Collections collection = Collections();
+  //Method();
+  //Collections collection = Collections();
 
-  collection.listAdd();
-  collection.listRemove();
-  collection.mapController();
-  collection.setController();
-  collection.practice();
+  //collection.listAdd();
+  //collection.listRemove();
+  //collection.mapController();
+  //collection.setController();
+  //collection.practice();
 
-  var conditional = Conditional();
-  conditional.simpleSwitch();
-  var ifScore = conditional.practiceIf(87);
-  var switchScore = conditional.practiceSwitch(91);
-  print('main ifScore = $ifScore, switchScore = $switchScore');
-  print(conditional.practiceIf(60));
+  //var conditional = Conditional();
+  //conditional.simpleSwitch();
+  //var ifScore = conditional.practiceIf(87);
+  //var switchScore = conditional.practiceSwitch(91);
+  //print('main ifScore = $ifScore, switchScore = $switchScore');
+  //print(conditional.practiceIf(60));
+
+  ForLoop();
 }
