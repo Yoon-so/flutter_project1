152bdb04e95bd909fae53a827af0a4c9cddc371d
Ekaterina <yoonrin113@gmail.com>
2025-08-28 14:14:25 +0900

    Update: member_list_screen.dart

--- lib/screen/network/memder_list_screen.dart
+++ lib/screen/network/memder_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project1/screen/network/json_model.dart';
+import 'package:flutter_project1/screen/network/member.dart';
 
 class MemberListScreen extends StatefulWidget {
   const MemberListScreen({super.key});
@@ -17,6 +18,8 @@ class _MemberListScreenState extends State<MemberListScreen> {
     ),
   );
 
+  List<Member> memberList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,20 +28,29 @@ class _MemberListScreenState extends State<MemberListScreen> {
         children: [
           ElevatedButton(
             onPressed: () {
-              dio.get('/api/v1/member/all');
+              dio.get('/api/v1/member/all').then((value) {
+                if (value.data is Iterable) {}
+
+                memberList =
+                    (value.data as Iterable)
+                        .map((e) => Member.fromJson(e))
+                        .toList();
+                print('memberList : $memberList');
+              });
             },
             child: Text("Get Data"),
           ),
-          ElevatedButton(
-            onPressed: () {
-              dio2.get("").then((value) {
-              print(value);
-              var jsonModel = JsonModel.fromJson(value.data);
-              print(jsonModel);
-              });
-            },
-            child: Text("Get Json"),
-          ),
+          // ElevatedButton(
+          //   onPressed: () {
+          //     dio2.get("").then((value) {
+          //       print(value);
+          //       var jsonModel = JsonModel.fromJson(value.data);
+          //       print(jsonModel);
+          //
+          //     });
+          //   },
+          //   child: Text("Get Json"),
+          // ),
         ],
       ),
     );
