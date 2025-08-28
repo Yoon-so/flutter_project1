8c005d75e67660dbd253c906f4fac74dd258efd1
Ekaterina <yoonrin113@gmail.com>
2025-08-28 14:05:23 +0900

    Add: json_model.dart

--- lib/screen/network/memder_list_screen.dart
+++ lib/screen/network/memder_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project1/screen/network/json_model.dart';
 
 class MemberListScreen extends StatefulWidget {
   const MemberListScreen({super.key});
@@ -9,19 +10,37 @@ class MemberListScreen extends StatefulWidget {
 }
 
 class _MemberListScreenState extends State<MemberListScreen> {
-
   Dio dio = Dio(BaseOptions(baseUrl: "https://d0a701c36a9e.ngrok-free.app"));
+  Dio dio2 = Dio(
+    BaseOptions(
+      baseUrl: "https://online-lecture-data.s3.ap-northeast-2.amazonaws.com/data.json",
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("멤버 조회")),
+    return Scaffold(
+      appBar: AppBar(title: Text("멤버 조회")),
       body: Column(
         children: [
-          ElevatedButton(onPressed: () {
-            dio.get('/api/v1/member/all');
-
-          }, child: Text("Get Data")),
+          ElevatedButton(
+            onPressed: () {
+              dio.get('/api/v1/member/all');
+            },
+            child: Text("Get Data"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              dio2.get("").then((value) {
+              print(value);
+              var jsonModel = JsonModel.fromJson(value.data);
+              print(jsonModel);
+              });
+            },
+            child: Text("Get Json"),
+          ),
         ],
-      ),);
+      ),
+    );
   }
 }
