85f2134a36b762f777ef2e4b90b80a107357eb93
Ekaterina <yoonrin113@gmail.com>
2025-08-28 15:41:50 +0900

    Update: member_register_screen.dart

--- lib/screen/network/network_screen.dart
+++ lib/screen/network/network_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project1/screen/network/memder_list_screen.dart';
+import 'package:flutter_project1/screen/network/member_list_screen.dart';
+import 'package:flutter_project1/screen/network/member_register_screen.dart';
 
 class NetworkScreen extends StatelessWidget {
   const NetworkScreen({super.key});
@@ -22,6 +23,26 @@ class NetworkScreen extends StatelessWidget {
               );
             },
             child: Text("회원 목록 조회"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return MemberRegisterScreen();
+                  },
+                ),
+              ).then((value) {
+                /// value가 null이 아니고
+                /// value가 bool 타입이고
+                /// value가 true인 경우
+                if(value != null && value is bool && value) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("등록 성공")));
+                }
+              },);
+            },
+            child: Text("회원 등록"),
           ),
         ],
       ),
